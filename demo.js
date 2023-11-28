d90667ec27a0b4164e4073fe3d7afe10c92eb462
Proton05 <jatinnimade05@gmail.com>
2023-11-29 02:13:54 +0530

    edit commit

--- demo.js
+++ demo.js
@@ -1,14 +1,79 @@
 
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-window.onload = function() {
-  var headerTitle = document.getElementById('header-title');
-  headerTitle.textContent = 'Hello World ' + headerTitle.textContent;
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete and Edit event
+itemList.addEventListener('click', handleButtonClick);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
-  // Add "Hello World" before "Item 1"
-  var itemsList = document.getElementById('items');
-  var firstItem = itemsList.firstElementChild;
-  var newListItem = document.createElement('li');
-  newListItem.textContent = 'Hello World ' + firstItem.textContent;
-  itemsList.insertBefore(newListItem, firstItem);
-  
+// Add item
+function addItem(e) {
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create delete button element
+  var deleteBtn = document.createElement('button');
+  // Add classes to delete button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Create edit button element
+  var editBtn = document.createElement('button');
+  // Add classes to edit button
+  editBtn.className = 'btn btn-primary btn-sm float-right edit';
+  // Append text node
+  editBtn.appendChild(document.createTextNode('Edit'));
+
+  // Append buttons to li
+  li.appendChild(deleteBtn);
+  li.appendChild(editBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
 }
+
+// Handle button click (delete or edit)
+function handleButtonClick(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure?')) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  } else if (e.target.classList.contains('edit')) {
+    // Handle edit button click (you can add the edit functionality here)
+    // For now, let's log a message to the console
+    console.log('Edit button clicked');
+  }
+}
+
+// Filter Items
+function filterItems(e) {
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
